e2a2210c9b3c5b4e5c85c1dc5e3e1ddc0f31248e
Namdev23 <nguyennamtiger@gmail.com>
2024-04-08 18:22:28 +0700

    New

--- main.js
+++ main.js
@@ -1,44 +1,44 @@
-import { dbFirestore } from "https://forio-source.github.io/foricon-firebase/script.js";
-import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
+// import { dbFirestore } from "https://forio-source.github.io/foricon-firebase/script.js";
+// import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
 
-const getForiconIcon = document.getElementById("getForiconIcon");
+// const getForiconIcon = document.getElementById("getForiconIcon");
 
-if (getForiconIcon == undefined) {
-  console.error("Cannot get import element");
-}
-else {
-  const u = getForiconIcon.getAttribute("userId");
-  async function get() {
-    try {
-      const docs = await getDoc(doc(dbFirestore, "users", u));
-      if (docs.exists()) {
-        const d = docs.data(), xhr = new XMLHttpRequest(), va = "";
-        if (d.foriconPlus == false) {
-          const css = await xhr.open("GET", "https://forio-source.github.io/nju199AHr81/73h9ER4juz85.txt", !0);
-          css.onreadystatechange = () => {
-            if (af.readyState == 4 && af.status == 200) {
-              let e = document.createElement("style");
-              e.innerHTML = xhr.responseText;
-              document.querySelector("head").appendChild(e);
-            }
-          }
-        }
-      }
-    }
-    catch (error) {
-      console.error(error);
-    }
-  };
-  get();
-}
-let f = document.querySelectorAll("foricon");
-f.forEach(async (e) => {
-  if (e.dataset.loadingtoggle != "false" || e.dataset.loadingtoggle == undefined) {
-    e.classList.add("0%18nl04d");
-    await wait(Math.round(40 * Math.random()) / 10);
-    e.classList.remove("0%18nl04d");
-  }
-});
+// if (getForiconIcon == undefined) {
+//   console.error("Cannot get import element");
+// }
+// else {
+//   const u = getForiconIcon.getAttribute("userId");
+//   async function get() {
+//     try {
+//       const docs = await getDoc(doc(dbFirestore, "users", u));
+//       if (docs.exists()) {
+//         const d = docs.data(), xhr = new XMLHttpRequest(), va = "";
+//         if (d.foriconPlus == false) {
+//           const css = await xhr.open("GET", "https://forio-source.github.io/nju199AHr81/73h9ER4juz85.txt", !0);
+//           css.onreadystatechange = () => {
+//             if (af.readyState == 4 && af.status == 200) {
+//               let e = document.createElement("style");
+//               e.innerHTML = xhr.responseText;
+//               document.querySelector("head").appendChild(e);
+//             }
+//           }
+//         }
+//       }
+//     }
+//     catch (error) {
+//       console.error(error);
+//     }
+//   };
+//   get();
+// }
+// let f = document.querySelectorAll("foricon");
+// f.forEach(async (e) => {
+//   if (e.dataset.loadingtoggle != "false" || e.dataset.loadingtoggle == undefined) {
+//     e.classList.add("0%18nl04d");
+//     await wait(Math.round(40 * Math.random()) / 10);
+//     e.classList.remove("0%18nl04d");
+//   }
+// });
 
 // let af = new XMLHttpRequest(), va = "";
 // import e from "./func173392.js";
@@ -65,3 +65,4 @@ f.forEach(async (e) => {
 //   af.send();
 // } while (af.readyState !== 4 && af.status !== 200);
 
+const _0x3096e5=_0x4cca;(function(_0x8deb1,_0xea65d7){const _0x5f68e=_0x4cca,_0x584a4a=_0x8deb1();while(!![]){try{const _0x4b0503=-parseInt(_0x5f68e(0x1ab))/0x1*(-parseInt(_0x5f68e(0x1bf))/0x2)+-parseInt(_0x5f68e(0x1a0))/0x3*(-parseInt(_0x5f68e(0x1a1))/0x4)+-parseInt(_0x5f68e(0x1aa))/0x5*(parseInt(_0x5f68e(0x1a4))/0x6)+parseInt(_0x5f68e(0x1b2))/0x7*(parseInt(_0x5f68e(0x1b3))/0x8)+-parseInt(_0x5f68e(0x1b7))/0x9+parseInt(_0x5f68e(0x1a5))/0xa+-parseInt(_0x5f68e(0x1b5))/0xb;if(_0x4b0503===_0xea65d7)break;else _0x584a4a['push'](_0x584a4a['shift']());}catch(_0x904742){_0x584a4a['push'](_0x584a4a['shift']());}}}(_0x3803,0x567a3));function _0x4cca(_0x1411fb,_0x3971ee){const _0x380349=_0x3803();return _0x4cca=function(_0x4ccaa9,_0x30a90a){_0x4ccaa9=_0x4ccaa9-0x19e;let _0x4b9f58=_0x380349[_0x4ccaa9];return _0x4b9f58;},_0x4cca(_0x1411fb,_0x3971ee);}import{dbFirestore}from'https://forio-source.github.io/foricon-firebase/script.js';import{doc,getDoc}from'https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js';const getForiconIcon=document[_0x3096e5(0x1af)](_0x3096e5(0x19e));function _0x3803(){const _0x3abab4=['getForiconIcon','querySelector','1332135KMdiTY','4zyuFYO','0%18nl04d','appendChild','8718iMIUOT','1919400tDXDuC','loadingtoggle','false','responseText','onreadystatechange','30jUcHfN','1NIlGSu','getAttribute','forEach','dataset','getElementById','createElement','querySelectorAll','1932qgcfhz','12352EvLiZa','foriconPlus','10058081DmOLdl','remove','2330748lHyiaC','status','random','GET','https://forio-source.github.io/nju199AHr81/73h9ER4juz85.txt','round','style','readyState','948286XCXWGp','head','add','users','classList'];_0x3803=function(){return _0x3abab4;};return _0x3803();}if(getForiconIcon==undefined)console['error']('Cannot\x20get\x20import\x20element');else{const u=getForiconIcon[_0x3096e5(0x1ac)]('userId');async function get(){const _0x4f42df=_0x3096e5;try{const _0x54b1f7=await getDoc(doc(dbFirestore,_0x4f42df(0x1c2),u));if(_0x54b1f7['exists']()){const _0x6dbb09=_0x54b1f7['data'](),_0x5328f3=new XMLHttpRequest(),_0x1d3d0b='';if(_0x6dbb09[_0x4f42df(0x1b4)]==![]){const _0x39725d=await _0x5328f3['open'](_0x4f42df(0x1ba),_0x4f42df(0x1bb),!0x0);_0x39725d[_0x4f42df(0x1a9)]=()=>{const _0x57d697=_0x4f42df;if(af[_0x57d697(0x1be)]==0x4&&af[_0x57d697(0x1b8)]==0xc8){let _0x1dbc47=document[_0x57d697(0x1b0)](_0x57d697(0x1bd));_0x1dbc47['innerHTML']=_0x5328f3[_0x57d697(0x1a8)],document[_0x57d697(0x19f)](_0x57d697(0x1c0))[_0x57d697(0x1a3)](_0x1dbc47);}};}}}catch(_0x3c485f){console['error'](_0x3c485f);}};get();}let f=document[_0x3096e5(0x1b1)]('foricon');f[_0x3096e5(0x1ad)](async _0x1089a1=>{const _0x4bc37d=_0x3096e5;(_0x1089a1['dataset'][_0x4bc37d(0x1a6)]!=_0x4bc37d(0x1a7)||_0x1089a1[_0x4bc37d(0x1ae)]['loadingtoggle']==undefined)&&(_0x1089a1[_0x4bc37d(0x1c3)][_0x4bc37d(0x1c1)](_0x4bc37d(0x1a2)),await wait(Math[_0x4bc37d(0x1bc)](0x28*Math[_0x4bc37d(0x1b9)]())/0xa),_0x1089a1['classList'][_0x4bc37d(0x1b6)](_0x4bc37d(0x1a2)));});
