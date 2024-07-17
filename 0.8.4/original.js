c4f0aae2aae2002a2a8cae4b3ed880c2b5cc89e4
Namdev23 <nguyennamtiger@gmail.com>
2024-07-17 22:42:33 +0700

    0.8.5

--- 0.8.4/original.js
+++ 0.8.4/original.js
@@ -26,109 +26,79 @@ else {
         month: date.getMonth() + 1,
         year: date.getFullYear(),
       };
-      if (!d.plan) {
-        d.plan = "lite";
-      };
+
       if (!d.pageviewStart) {
         d.pageviewStart = crnt;
       };
-      if (!d.pageviewCount) {
-        d.pageviewCount = 1;
-      };
       if (crnt.month > d.pageviewStart.month || crnt.year > d.pageviewStart.year) {
         d.pageviewStart = crnt;
-        d.pageviewCount = 0;
+        d.pageviewCount = 1;
       }
-      else {
-        d.pageviewCount++;
-      };
-      console.log(d.pageviewCount);
-      console.log(plans[d.plan])
+      else d.pageviewCount++;
 
       try {
         await setDoc(doc(dbFirestore, 'users', u[1]), {
           pageviewCount: d.pageviewCount,
           pageviewStart: d.pageviewStart,
-          plain: d.plan,
         }, {merge: true});
       }
       catch (error) {
         error(error);
       };
 
-      if (d.settings.allowedDomains.some((domain) => location.hostname == domain)) {
-        customElements.define(
-          "f-icon",
-          class extends HTMLElement {
-            getIcon() {
-              return this.getAttribute("icon");
-            }
-            setIcon(iconName) {
-              if (iconName) this.setAttribute("icon", iconName);
-              else error("Argument cannot be null or undefined");
-            }
-            toggleIcon(icon1Name, icon2Name) {
-              if (arguments.length == 2) {
-                this.getAttribute("icon") == icon1Name ? this.setAttribute("icon", icon2Name) : this.setAttribute("icon", icon1Name);
+      if (d.pageviewCount > plans[d.plan]) console.error("You have reached the limit of pageviews");
+      else {
+        if (d.settings.allowedDomains.some((domain) => location.hostname == domain)) {
+          customElements.define(
+            "f-icon",
+            class extends HTMLElement {
+              getIcon() {
+                return this.getAttribute("icon");
               }
-              else error("There are must be 2 arguments");
-            }
-            getStyle() {
-              let splited = this.getAttribute("type").split("/");
-              return splited.length == 2 ? splited[1] : splited[0];
-            }
-            toggleStyle() {
-              let spl = this.getAttribute("type");
-              spl = spl ? spl.split("/") : ["filled"];
-              let tst = spl.length == 2 ? `${spl[0]}/${spl[1] == "filled" ? "outlined" : "filled"}` : `${spl[0] == "filled" ? "outlined" : "filled"}`;
-              this.setAttribute("type", tst);
-            }
-            getType() {
-              let spl = this.getAttribute("type");
-              spl = spl ? spl.split("/") : [""];
-              return spl.length == 2 ? spl[0] : "regular";
-            }
-            setType(typeName) {
-              if (typeName || typeName == "") {
-                let spl = this.getAttribute("type");
-                spl = spl ? spl.split("/") : [""];
-                let style = spl.length == 2 ? spl[1] : spl[0] == "" ? "filled" : spl[0];
-                let tst = typeName == "" ? style : `${typeName}/${style}`;
-                this.setAttribute("type", tst);
+              setIcon(iconName) {
+                if (typeof iconName == 'string') this.setAttribute("icon", iconName);
+                else error("Argument must be a valid string");
               }
-              else error("Argument cannot be null or undefined");
-            }
-            toggleType(type1Name, type2Name) {
-              if (arguments.length == 2) {
-                let spl = this.getAttribute("type");
-                spl = spl ? spl.split("/") : [""];
-                spl = spl.length == 2 ? spl : ["", ...spl];
-                let tst = spl[0] == type1Name ? type2Name : type1Name;
-                if (tst != "") tst += "/";
-                this.setAttribute("type", `${tst}${spl[1] == "" ? "filled" : spl[1]}`);
+              toggleIcon(icon1Name, icon2Name) {
+                if (arguments.length == 2) {
+                  this.getAttribute("icon") == icon1Name ? this.setAttribute("icon", icon2Name) : this.setAttribute("icon", icon1Name);
+                }
+                else error("There are must be 2 arguments");
               }
-              else error("There are must be 2 arguments");
+              getStyle() {
+                return this.getAttribute("icon-style");
+              }
+              setStyle(styleName) {
+                if (typeof iconName == 'string') this.setAttribute("icon-style", styleName);
+                else error("Argument must be a valid string");
+              }
+              toggleType(style1Name, style2Name) {
+                if (arguments.length == 2) {
+                  this.getAttribute("icon-style") == icon1Name ? this.setAttribute("icon-style", style2Name) : this.setAttribute("icon-style", style1Name);
+                }
+                else error("There are must be 2 arguments");
+              }
             }
-          }
-        );
-        
-        const xhr = new XMLHttpRequest();
-        xhr.open(
-          "GET",
-          "//foricon-src.github.io/main/0.8.4/style.css",
-          !0
-        );
-        xhr.onreadystatechange = () => {
-          if (xhr.readyState == 4 && xhr.status == 200) {
-            let s = document.createElement("style");
-            s.innerHTML = xhr.responseText;
-            document.querySelector("head").appendChild(s);
-          }
-        };
-        do xhr.send();
-        while (xhr.readyState != 4 && xhr.status != 200);
-      }
-      else error("This site cannot access to Foricon assets because the user has't allowed this domain yet");
+          );
+          
+          const xhr = new XMLHttpRequest();
+          xhr.open(
+            "GET",
+            "//foricon-src.github.io/main/0.8.4/style.css",
+            !0
+          );
+          xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+              let s = document.createElement("style");
+              s.innerHTML = xhr.responseText;
+              document.querySelector("head").appendChild(s);
+            }
+          };
+          do xhr.send();
+          while (xhr.readyState != 4 && xhr.status != 200);
+        }
+        else error("This site cannot access to Foricon assets because the user has't allowed this domain yet");
+      };
     } 
     else error("Given account id is invalid");
   })();
